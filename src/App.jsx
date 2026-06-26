7293fc32377eb023a7823bd82073fc3792886eff
Emily Hua <145943828+abcdemilyfgh@users.noreply.github.com>
2026-06-25 19:39:57 -0700

    added home, untangle, focus, and rant pages + nav to each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 import Untangle from './pages/Untangle'
 import Focus from './pages/Focus'
 import Rant from './pages/Rant'
@@ -6,6 +6,13 @@ import Rant from './pages/Rant'
 function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/untangle"> Untangle </Link>
+        <Link to="/focus"> Focus </Link>
+        <Link to="/rant"> Rant </Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/untangle" element={<Untangle />} />
