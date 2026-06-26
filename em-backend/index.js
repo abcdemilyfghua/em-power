68ebb88d867203a49334d6623308f61e3482254f
Emily Hua <145943828+abcdemilyfgh@users.noreply.github.com>
2026-06-26 01:05:01 -0700

    feat: made GET endpoint

--- em-backend/index.js
+++ em-backend/index.js
@@ -1,5 +1,21 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
+const { createClient } = require('@supabase/supabase-js');
+const SUPABASE_URL = process.env.SUPABASE_URL
+const SUPABASE_KEY = process.env.SUPABASE_KEY
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+app.get('/tasks', async (req, res) => {
+    const { data, error } = await supabase
+        .from('tasks')
+        .select('*')
+
+    if (error) return res.status(500).json({ error: error.message })
+    res.json(data)
+})
 
 app.listen(3000, () => {
     console.log('server running on port 3000')
