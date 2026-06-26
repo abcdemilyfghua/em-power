581ff9862f27b70fde70d5b0bb30626604ed9f9f
Emily Hua <145943828+abcdemilyfgh@users.noreply.github.com>
2026-06-25 20:52:53 -0700

    feat: add controlled form to untangle page with name, deadline and notes fields

--- src/pages/Untangle.jsx
+++ src/pages/Untangle.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react"
+
 function Untangle() {
-    return <h1>Untangle</h1>
+
+    const [name, setName] = useState('')
+    const [time, setTime] = useState('')
+    const [notes, setNotes] = useState('')
+
+    return (
+        <>
+            <h1>Untangle</h1>
+
+            <label htmlFor="name">Task Name:</label>
+            <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+
+            <label htmlFor="time">Deadline to complete:</label>
+            <input type="datetime-local" id="time" value={time} onChange={(e) => setTime(e.target.value)} />
+
+            <label htmlFor="notes">Notes:</label>
+            <textarea type="text" id="notes" value={notes} onChange={(e) => setNotes(e.target.value)} />
+        </>
+    )
 }
 
 export default Untangle
